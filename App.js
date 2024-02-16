b27f1fb9bb494c0c00157873f9988a2945c27e3c
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-02-16 11:49:21 +0530

    React Basic

---
+++ App.js
@@ -0,0 +1,58 @@
+{/* <div class="parent">
+    <div class="child">
+<h1>I am h1 tag</h1>
+<h1>I am h2 tag</h1>
+    </div>
+     <div class="child2">
+<h1>I am h1 tag</h1>
+<h1>I am h2 tag</h1>
+    </div>
+</div> */}
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+  [  React.createElement(
+        "div",
+        {id:"child"},
+      [  React.createElement(
+            "h1",
+            {},
+            "I am h1 Tag"
+            ),React.createElement(
+                "h2",
+                {},
+                "I am h2 Tag"
+                )]
+            
+            
+            ),
+
+            React.createElement(
+                "div",
+                {id:"child2"},
+              [  React.createElement(
+                    "h1",
+                    {},
+                    "I am h1 Tag"
+                    ),React.createElement(
+                        "h2",
+                        {},
+                        "I am h2 Tag"
+                        )]
+                    
+                    
+                    )
+        
+        ]
+            
+            
+            )
+
+
+// const heading=React.createElement("h1",{id:"heading",xyz:"abs"},"Hello world from React!")
+
+// createElement(TagName,{attribute of tag},This to add in tag||children)
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent)
