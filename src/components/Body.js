feff1b3075bcc2c63b7cdbf6e9470c2dc9cfcda6
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-02-22 13:10:32 +0530

    filter restaurant list

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,22 @@
 import RestroCard from "./RestroCard";
 import { restroObj } from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [displayData, setDisplayData] = useState(restroObj);
+
+  const handleRestaurantList = () => {
+    let list = displayData.filter((ele) => ele.info.avgRating > 4.2);
+    setDisplayData(list);
+  };
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button onClick={handleRestaurantList}>Top Rated Restaurant</button>
+      </div>
       <div className="restro-container">
         {/* //restrocard */}
-        {restroObj.map((ele, index) => (
+        {displayData.map((ele, index) => (
           <RestroCard restroData={ele} key={ele.info.id} />
         ))}
       </div>
