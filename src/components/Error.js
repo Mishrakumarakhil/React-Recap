49bb54ae88fafa0b69439a2779fbadb9c6cf969c
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-02-27 14:03:41 +0530

    useError

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,11 @@
+import { useRouteError } from "react-router-dom";
+
 const Error = () => {
+  const err = useRouteError();
   return (
     <div>
       <h1>Oops!!!</h1>
-      <p>somethign went wrong.</p>
+      <p>{err.data}</p>
     </div>
   );
 };
