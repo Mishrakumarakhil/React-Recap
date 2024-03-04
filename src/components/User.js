80d7bc5a0c5e636b8a4ee37bfeae0d553429ebe5
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-04 11:43:42 +0530

    added state in class

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,10 @@
+import { useState } from "react";
+
 const User = ({ name }) => {
+  const [count] = useState(0);
   return (
     <div className="user-card">
+      <h1>Count-{count}</h1>
       <div>Name:{name}</div>
       <div>location:Bangalore</div>
       <div>Contact:akhil184</div>
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -2,10 +2,14 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      count: 0,
+    };
   }
   render() {
     return (
       <div className="user-card">
+        <h1>Count-{this.state.count}</h1>
         <div>Name:{this.props.name}</div>
         <div>location:Bangalore</div>
         <div>Contact:akhil184</div>
