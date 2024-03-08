0fb2fb99f6396a5b3a711e89b67e7374b9eec038
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-08 12:47:11 +0530

    updated icon

--- src/components/RestaturantCategory.js
+++ src/components/RestaturantCategory.js
@@ -16,7 +16,7 @@ const RestaurantCategory = ({ data, showItems, setShowIndex }) => {
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length}){" "}
           </span>
-          <span>⬇️</span>
+          <span>{showItems ? "⬆️" : "⬇️"}</span>
         </div>
         {/* Accordian Body */}
         {showItems && <ItemList items={data.itemCards} />}
