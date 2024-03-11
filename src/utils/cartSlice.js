1617384dc8e132f0435ca78e8e912100d1fb746b
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-11 14:03:43 +0530

    configuired slice in store

--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -14,7 +14,7 @@ const cartSlice = createSlice({
       state.items.pop();
     },
     clearCart: (state, action) => {
-      state.items.length = 0;
+      state.items.length = 0; //[]
     },
   },
 });
