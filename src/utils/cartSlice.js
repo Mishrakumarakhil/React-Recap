f90c9de6d37516fa12aa1effe720b7fc19ab0fb7
Mishrakumarakhil <akhilmishra184@gmail.com>
2024-03-11 13:59:23 +0530

    created slice

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initalState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      //Modify the slice of store
+      state.items.push(action.payload);
+    },
+    removeItem: (state, action) => {
+      state.items.pop();
+    },
+    clearCart: (state, action) => {
+      state.items.length = 0;
+    },
+  },
+});
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
